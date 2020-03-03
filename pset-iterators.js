912aa997ac2b902da62ce47a246e97e05cab63a4
Matthew Cillis <MattCillis@Matts-MacBook-Pro.local>
2020-03-03 18:07:18 -0500

    Matt Cillis- Completed HW

--- pset-iterators.js
+++ pset-iterators.js
@@ -42,7 +42,11 @@ Expected Result Shape: A string representing the full name of the president that
 console.log('Problem 1:')
 
 // Add your code below this line
-
+//use find on presidents.party
+const firstWhigPresident = presidents.find((president) =>{
+  return president.party === 'Whig'
+})
+console.log(firstWhigPresident)
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -65,6 +69,16 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 2:')
 
 // Add your code below this line
+//use filter to focus on each president's names
+const presidentsNamedJames = presidents.filter((president) => {
+  //use split to separate the names, use index of 0 to access the first name
+  //use === to check if the first name is equal to james
+  return president.president.split(' ')[0] === 'James'
+})
+//console log result - works!
+console.log(presidentsNamedJames)
+
+
 
 // Add your code above this line
 
@@ -92,7 +106,11 @@ Tip: your new array should contain 45 elements
 console.log('Problem 3:')
 
 // Add your code below this line
-
+// use map to create an array of presidential parties
+const presidentialParties = presidents.map((president) => {
+  return president.party
+})
+console.log(presidentialParties)
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -114,6 +132,15 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 4:')
 
 // Add your code below this line
+//create an array of the year presidents took office by using slice on the took_office attribute
+const presidentsBetween1850and1900 = presidents.filter((president) => {
+  //parseInt the year they took office and make sure that it's greater than 1850
+  return (parseInt(president.took_office.slice(0, 4)) > 1850) &&
+  //add in logical and to make sure it's <1900
+  (parseInt(president.took_office.slice(0, 4)) < 1900)
+})
+//check the result- it works!
+console.log(presidentsBetween1850and1900)
 
 // Add your code above this line
 
@@ -137,7 +164,11 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 5:')
 
 // Add your code below this line
-
+//use filter to find the presidents who's death_year = 'null'
+const livingPresidents = presidents.filter((president) => {
+ return president.death_year === null
+})
+console.log(livingPresidents)
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -160,7 +191,14 @@ Expected Result Shape: A string representing the full name of the president that
 console.log('Problem 6:')
 
 // Add your code below this line
-
+//use find to get the first republican president
+const firstRepublicanAttributes = presidents.find((president) => {
+  //get the first president
+  return president.party === 'Republican'
+})
+  //extract the first Republican's name using .president
+const firstRepublican = firstRepublicanAttributes.president
+console.log(firstRepublican)
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -183,8 +221,22 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 7:')
 
 // Add your code below this line
+//similar to problem 4, use slice to access year took office and year left office, parseInt result
+//create a new array first without trump using filter
+const noMoreTrump = presidents.filter((president) => {
+  return president.president != 'Donald J. Trump'
+})
+// check if it works- it does!
+// console.log(noMoreTrump)
 
-// Add your code above this line
+const shortTermPresidents = noMoreTrump.filter((president) => {
+  //use the array wihtout trump
+  //subtract the value of when the president took office to when they left office, only log the presidents that that number is less than 4 for
+  return (parseInt(president.left_office.slice(0, 4)) - parseInt(president.took_office.slice(0,4))) < 4
+})
+console.log(shortTermPresidents)
+
+
 
 /** added for formatting purposes **/
 console.log('')
@@ -210,6 +262,35 @@ Example (not the actual answer):
 console.log('Problem 8:')
 
 // Add your code below this line
+
+// create presidential first names array
+const presidentFirstName = presidents.map((president) => {
+  return president.president.split(' ')[0]
+})
+// check result- it works!
+// console.log(presidentFirstName)
+
+function presidentFirstNameCounter (name) {
+
+  return presidentFirstName.reduce((nameCount, name) => {
+    if (nameCount[name] === undefined) {
+      // name does not exist in presidentFirstNameObj
+      nameCount[name] = 1
+    } else {
+      // when name already exists in presidentFirstNameObj
+      nameCount[name] += 1
+    }
+
+    return nameCount
+  }, {})
+}
+// check to see if function works, it does
+// console.log(presidentFirstNameCounter())
+
+//create variable named firstNameCount
+const firstNameCount = presidentFirstNameCounter()
+
+console.log(firstNameCount)
 
 
 
